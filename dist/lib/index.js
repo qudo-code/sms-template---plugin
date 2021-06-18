09f8cc626f4a805a6546532ac9b8fa5456f25468
Lucas Hugdahl <lucashugdahl@Lucass-MacBook-Air.local>
2021-06-18 16:21:07 -0700

    feat: initial

--- dist/lib/index.js
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});exports.default=(e={})=>({config:(e=!1)=>{},service:(e=!1,r=!1)=>{}}),exports.exampleHelper=()=>{};
--- dist/lib/index.mjs
+++
@@ -1,1 +0,0 @@
-const e=()=>{};export default(e={})=>({config:(e=!1)=>{},service:(e=!1,o=!1)=>{}});export{e as exampleHelper};
